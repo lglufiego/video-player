d88712958daf056ca01298a531e4fa64b6487aa9
Gustavo <lg.lufiego@gmail.com>
2026-03-11 18:25:15 -0300

    v.07 css edit, js created. playVideo function created.

---
+++ script.js
@@ -0,0 +1,31 @@
+const video_player = document.querySelector("#video_player")
+const mainVideo = video_player.querySelector("#main-video")
+const pogressionAreaTime = video_player.querySelector(".progressAreaTime")
+const controls = video_player.querySelector(".controls")
+const progressArea = video_player.querySelector(".progress-area")
+const progress_Bar = video_player.querySelector(".progress-bar")
+const fast_rewind = video_player.querySelector(".fast-rewind")
+const play_pause = video_player.querySelector(".play-pause")
+const fast_forward = video_player.querySelector(".fast-forward")
+const volume = video_player.querySelector(".volume")
+const volume_range = video_player.querySelector(".volume_range")
+const current = video_player.querySelector(".current")
+const duration = video_player.querySelector(".duration")
+const auto_play = video_player.querySelector(".auto-play")
+const settingsBtn = video_player.querySelector(".settingsBtn")
+const picture_in_picture = video_player.querySelector(".picture_in_picture")
+const fullscreen = video_player.querySelector(".fullscreen")
+const settings = video_player.querySelector("#settings")
+const playback = video_player.querySelector("#playback")
+
+function playVideo() {
+  play_pause.innerHTML = "pause";
+  play_pause.innerHTML = "play";
+  video_player.classList.add("paused");
+}
+
+play_pause.addEventListener("click", () => {
+  playVideo()
+});
+
+// 41:31
